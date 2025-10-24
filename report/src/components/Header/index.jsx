0917e7e1f021b8ef721087d34bc07ad4f0141eb6
Orisha-Obt <ramoroketsele@gmail.com>
2025-10-24 22:55:41 +0200

    Report first changes

---
+++ report/src/components/Header/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="w-full bg-red-500 flex p-2">
+      <div className="w-full flex flex-col justify-center items-center text-center">
+        <h1 className="text-sm font-semibold">
+          Mokonyane is a platform maintained by mochalla hackesrs
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ report/src/components/Navbar/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const navItems = [
+    {
+      id: 1,
+      title: "Report",
+      path: "/report",
+    },
+    {
+      id: 2,
+      title: "Statistics",
+      path: "/stats",
+    },
+    {
+      id: 3,
+      title: "About",
+      path: "/about",
+    },
+    {
+      id: 4,
+      title: "Contacts",
+      path: "/contacts",
+    },
+  ];
+  return (
+    <div className="w-full bg-slate-500 p-2 h-16">
+      <div className="flex justify-between items-center">
+        <div>
+          <Link to="/" className="font-bold">
+            mokonyane
+          </Link>
+        </div>
+        <div className="flex mr-4">
+          <ul className="flex p-1 gap-2">
+            {navItems.map((link) => (
+              <li>
+                <Link to={link.path}>{link.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
